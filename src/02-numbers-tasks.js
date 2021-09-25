8af87579d6756c22816af42d79a7d253a2ede2dc
DavitBokuchava <davitbokuchava@gmail.com>
2021-09-25 15:15:46 +0400

    feat: 56 passing

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -186,8 +186,9 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  // throw new Error('Not implemented');
+  return Math.round(num / (10 ** pow)) * (10 ** pow);
 }
 
 /**
