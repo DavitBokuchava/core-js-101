26667202a6e8998cae896e562847a9cf82901599
DavitBokuchava <davitbokuchava@gmail.com>
2021-09-28 23:31:43 +0400

    feat: 92 passed

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -58,8 +58,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  // eslint-disable-next-line no-param-reassign,arrow-body-style,no-plusplus
+  return new proto.constructor(...Object.values(JSON.parse(json)));
 }
 
 
